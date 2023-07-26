f44f6ab59a0a13f4b01accf3e1f6503e38af75e1
jacob-d242 <jacobmoracha@gmail.com>
2023-07-26 20:52:39 +0300

    coomputed in vue

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,95 @@
 <script setup>
-import { ref } from 'vue'
+import { ref,computed } from 'vue'
 
+
+const listItems = ref([
+   {
+     id: 1, 
+     label: "10 party hats", 
+     purchased: true, 
+     highPriority: false
+   },
+   {
+		id: 2, 
+    label:"2 board games", 
+    purchased: true, 
+    highPriority: false
+   },
+   {
+     id: 3, 
+     label: "20 cups", 
+     purchased: false, 
+     highPriority: true
+   }
+])
 const msg = ref('Shopping List App!')
+const characterCount = computed(()=>{
+  return newItem.value.length
+}) 
 const newItem = ref('')
 const newItemHighPriority = ref(false)
 const editing = ref(false);
-const listItems= ref ([
-//   { id : 1 ,label :"10 milk shakes "},
-//  { id:2, label:"2 milk cakes"},
-//  { id : 3 , label :"20 glasses"}
-])
-const saveItem=()=>{
-  listItems.value.push({id: listItems.value.length + 1 ,label:newItem.value})
-  newItem.value = ""
+const saveItem = () => {
+  listItems.value.push(
+    {
+    id: listItems.value.length + 1,
+    label: newItem.value,
+    highPriority: newItemHighPriority.value,
+    purchased: false
 }
-const editItem = (e) =>{
+  )
+  newItem.value = ""
+  // newItemHighPriority.value = ""
+}
+
+const editItem = (e) => {
   editing.value = e
   newItem.value = ""
+ // newItemHighPriority.value =""
+}
+
+const togglePurchase = (item) => {
+  item.purchased = !item.purchased
 }
 </script>
-  
-<template>
-  <div class="header"> 
-    <h1>{{ msg }}</h1>
-    <button v-if="editing" class="btn btn-primary" @click="editItem(false)" >Cancel</button>
-    <button v-else class="btn btn-primary" @click="editItem(true)" >Add New</button>
-  </div>
-  <a v-bind:href="newItem">Dynamic Link</a>
-  <form 
-  v-if="editing "
-   @submit.prevent="saveItem" 
-   class ="add-item-form">
 
-    <input type="text" placeholder="add Item" v-model.trim="newItem"/>
+<template>
+  <div class="header">
+    <h1>{{ msg }}</h1>
+    <button v-if="editing" class="btn btn-primary" @click="editItem(false)">Cancel</button>
+    <button v-else class="btn btn-primary" @click="editItem(true)">Add New</button>
+  </div>
+  <form
+    :v-if="editing"
+    @submit.prevent="saveItem"
+    class="add-item-form"
+  >
+
+    <input type="text" placeholder="add Item" v-model.trim="newItem" />
     <label>
-      <input type="checkbox"
-      v-model="newItemHighPriority"/>
+      <input type="checkbox" v-model="newItemHighPriority" value="newItemHighPriority"/>
       High Priority
     </label>
-    <button 
-    :disabled="newItem.length === 0"
-    class="btn btn-primary"
-    >
-    Add to list</button> 
-</form>
+    <button :disabled="newItem.length === 0" class="btn btn-primary">
+      Add to list
+    </button>
+  </form>
+  <p>{{ characterCount }}/100</p>
   <ul>
-    <li v-for="{id,label}, in listItems" :key="id">{{label }}</li>
+    <li
+      v-for="( item, index ) in listItems"
+      @click="togglePurchase(item)"
+      class="static-class"
+      :key="item.id"
+      :class="{
+        strikeout: item.purchased,
+        priority: item.highPriority
+      }"
+    >
+      {{ item.label }}
+    </li>
   </ul>
-  <p v-if="!listItems.length ">
+  <p v-if="!listItems.length">
     No Items available please add in the above
   </p>
 </template>
